fc0dd021619a640cec0ecac0fd310bbe1e90bb20
msr <mcphlen@gmail.com>
2024-03-31 03:30:07 -0400

    Introduced AiClientBase

---
+++ SimpleOpenAiService/Clients/AiClientBase.cs
@@ -0,0 +1,35 @@
+﻿using SimpleOpenAiService.Hubs;
+using static SimpleOpenAiService.Hubs.AiChatHub;
+
+namespace SimpleOpenAiService.Clients;
+
+public abstract class AiClientBase
+{
+    private readonly AiChatHub _aiChatHub;
+
+    protected AiClientBase(AiChatHub aiChatHub)
+    {
+        _aiChatHub = aiChatHub;
+        OnInitialized();
+    }
+
+    public CancellationTokenSource CancellationSource = new();
+
+    public abstract Task StreamCompletion(string prompt, CancellationToken cancellationToken);
+
+    public abstract Task EnsureModelExists(bool mustPullModel, CancellationToken cancellationToken);
+
+    private void OnInitialized() => _aiChatHub.UserMessageReceived += OnUserMessageReceived;
+
+    private async void OnUserMessageReceived(object? sender, UserMessageReceivedEventArgs e)
+    {
+        try
+        {
+            await StreamCompletion(e.Prompt, CancellationSource.Token);
+        }
+        catch (HttpRequestException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            // todo: polly, model not ready yet
+        }
+    }
+}
--- SimpleOpenAiService/Clients/OllamaClient.cs
+++ SimpleOpenAiService/Clients/OllamaClient.cs
@@ -1,55 +1,69 @@
 ﻿using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using Meziantou.Framework;
 using Microsoft.Extensions.Options;
 using OllamaSharp;
 using OllamaSharp.Models;
-using OllamaSharp.Streamer;
 using SimpleOpenAiService.Configuration;
 using SimpleOpenAiService.Hubs;
-using static SimpleOpenAiService.Hubs.AiChatHub;
 
 namespace SimpleOpenAiService.Clients;
 
-public sealed class OllamaClient
-{
-    public OllamaClient(
+public sealed class OllamaClient(
         ILogger<OllamaClient> logger,
         IOptions<OllamaConfiguration> ollamaConfig,
         AiChatHub aiChatHub)
-    {
-        _logger = logger;
-        _ollamaConfig = ollamaConfig;
-        _aiChatHub = aiChatHub;
-        _ollamaApi = new(ollamaConfig.Value.Uri, ollamaConfig.Value.Model);
+    : AiClientBase(aiChatHub)
+{
+    private readonly OllamaApiClient _ollamaApi = new(ollamaConfig.Value.Uri, ollamaConfig.Value.Model);
+    private readonly ILogger<OllamaClient> _logger = logger;
+    private readonly AiChatHub _aiChatHub = aiChatHub;
+    private Queue<string> _unansweredPrompts = new();
 
-        _aiChatHub.UserMessageReceived += OnUserMessageReceived;
+    public override async Task StreamCompletion(string prompt, CancellationToken cancellationToken)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            if (_unansweredPrompts.Count < 3)
+            {
+                _unansweredPrompts.Enqueue(prompt);
+            }
+
+            return;
+        }
+
+        while (_unansweredPrompts.Count > 0)
+        {
+            var unanswered = _unansweredPrompts.Dequeue();
+            await StreamCompletion(unanswered, cancellationToken);
+        }
+
+        List<string> answerDebug = [];
+
+        ConversationContext? context = null;
+        context = await _ollamaApi.StreamCompletion(
+            prompt,
+            context,
+            async stream =>
+            {
+                answerDebug.Add(stream.Response);
+
+                await _aiChatHub.SendBotMessage("bot", stream.Response);
+
+                if (stream.Done)
+                {
+                    await _aiChatHub.SendBotMessage("bot", "%%%DONE%%");
+                }
+            },
+            cancellationToken);
+
+        _logger.LogInformation(string.Join("", answerDebug));
     }
 
-    public CancellationTokenSource CancellationSource = new();
-
-    private async void OnUserMessageReceived(object? sender, UserMessageReceivedEventArgs e)
-    {
-        try
-        {
-            _ = await StreamCompletion(e.Prompt, CancellationSource.Token);
-        }
-        catch (HttpRequestException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
-        {
-            // todo: polly, model not ready yet
-        }
-    }
-
-    private readonly OllamaApiClient _ollamaApi;
-    private readonly ILogger<OllamaClient> _logger;
-    private readonly IOptions<OllamaConfiguration> _ollamaConfig;
-    private readonly AiChatHub _aiChatHub;
-
-    public async Task EnsureModelExists(bool mustPullModel, CancellationToken cancellationToken)
+    public override async Task EnsureModelExists(bool mustPullModel, CancellationToken cancellationToken)
     {
         IEnumerable<Model> localModels = await EnsureOllamaIsRunning(cancellationToken);
 
-        string modelName = _ollamaConfig.Value.Model;
+        var modelName = ollamaConfig.Value.Model;
 
         if (localModels.Any(m =>
         {
@@ -69,6 +83,27 @@ public sealed class OllamaClient
         await PullModel(cancellationToken);
 
         // Local Function(s)
+        async Task<IEnumerable<Model>> EnsureOllamaIsRunning(CancellationToken cancellationToken)
+        {
+            IEnumerable<Model> localModels = [];
+            bool isOllamaRunning = false;
+            do
+            {
+                try
+                {
+                    localModels = await _ollamaApi.ListLocalModels(cancellationToken);
+                    isOllamaRunning = true;
+                }
+                catch (HttpRequestException e)
+                {
+                    _logger.LogError($"Ollama is not running: {e.Message}");
+                    await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken);
+                }
+            } while (!isOllamaRunning);
+
+            return localModels;
+        }
+
         async Task PullModel(CancellationToken cancellationToken)
         {
             _logger.LogInformation($"Pulling model '{modelName}'...");
@@ -90,53 +125,6 @@ public sealed class OllamaClient
                 return;
             }
         }
-
-        async Task<IEnumerable<Model>> EnsureOllamaIsRunning(CancellationToken cancellationToken)
-        {
-            IEnumerable<Model> localModels = [];
-            bool isOllamaRunning = false;
-            do
-            {
-                try
-                {
-                    localModels = await _ollamaApi.ListLocalModels(cancellationToken);
-                    isOllamaRunning = true;
-                }
-                catch (HttpRequestException e)
-                {
-                    _logger.LogError($"Ollama is not running: {e.Message}");
-                    await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken);
-                }
-            } while (!isOllamaRunning);
-
-            return localModels;
-        }
-    }
-
-    public async Task<IEnumerable<string>> StreamCompletion(string prompt, CancellationToken cancellationToken)
-    {
-        List<string> answer = [];
-
-        ConversationContext? context = null;
-        context = await _ollamaApi.StreamCompletion(
-            prompt,
-            context,
-            async stream =>
-            {
-                answer.Add(stream.Response);
-
-                await _aiChatHub.SendBotMessage("bot", stream.Response);
-
-                if (stream.Done)
-                {
-                    await _aiChatHub.SendBotMessage("bot", "%%%DONE%%");
-                }
-            },
-            cancellationToken);
-
-        _logger.LogInformation(string.Join("", answer));
-
-        return answer;
     }
 
     //public async IAsyncEnumerable<string> StreamCompletion2(string prompt, [EnumeratorCancellation] CancellationToken cancellationToken)
